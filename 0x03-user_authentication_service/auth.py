34f69e563b96aacef673039045eded5ffde5090a
AbYahaya <yabdulrauf911@gmail.com>
2024-11-21 12:51:51 +0000

    more implementations

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,20 +1,47 @@
 #!/usr/bin/env python3
 """
-Authentication module
+Auth module
 """
-import bcrypt
+from db import DB
+from user import User
+from bcrypt import hashpw, gensalt
+from sqlalchemy.exc import NoResultFound
+from typing import Union
+
 
 def _hash_password(password: str) -> bytes:
-    """Hashes a password with bcrypt and returns the salted hash.
+    """Hash a password with bcrypt."""
+    return hashpw(password.encode('utf-8'), gensalt())
 
-    Args:
-        password (str): The password to hash.
 
-    Returns:
-        bytes: The salted hash of the password.
-    """
-    if not isinstance(password, str):
-        raise TypeError("Password must be a string.")
-    salt = bcrypt.gensalt()  # Generate a salt
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)  # Hash the password
-    return hashed_password
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user with the given email and password.
+
+        Args:
+            email (str): User's email.
+            password (str): User's password.
+
+        Raises:
+            ValueError: If a user with the given email already exists.
+
+        Returns:
+            User: The newly created User object.
+        """
+        try:
+            # Check if user already exists
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # If user does not exist, create a new one
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(
+                email=email,
+                hashed_password=hashed_password.decode('utf-8'),
+            )
+            return new_user
